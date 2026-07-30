13556382f87c3cae46e3c554bf3cbf2e6914848d
Isaac Njenga <isaac.njenga@mediacent.africa>
2026-07-30 16:14:10 +0300

    no other way

--- front/src/pages/Quotations/Quotations.js
+++ front/src/pages/Quotations/Quotations.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-function Quotations() {
-  return (
-    <div>Quotations</div>
-  )
+export default function Quotations() {
+  return <div>Quotations</div>;
 }
-
-export default Quotations
